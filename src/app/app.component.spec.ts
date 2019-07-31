cd862f45e5acd37e37dd889004bddacda008ae38
LeoLanese <leolanese@macbook.local>
2019-07-31 21:36:27 +0100

    updating parts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'spur-template-for-angular'`, () => {
+  it(`should have as title 'portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('spur-template-for-angular');
+    expect(app.title).toEqual('portfolio');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to spur-template-for-angular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Portfolio');
   });
 });
